45cd292ae9352724cccf52ed562a950dfad5afff
Alex Wailan <alex.wailan24@gmail.com>
2019-12-20 14:33:58 +1000

    Update purge.py

--- purge.py
+++ purge.py
@@ -33,9 +33,9 @@ def getargv():
 	parser = argparse.ArgumentParser(usage=usage, description=description)
 
 	parser.add_argument('aln_file', help='Core SNV alignment file', metavar='FILE',type=str, nargs='?')
-	parser.add_argument('masking_file', help='The masking files.', metavar='FILE', type=str, nargs='?')
-	parser.add_argument('-d',	'--dirpath', help='Specify input directory containing files. End with a forward slash. Eg. /temp/fasta/', metavar='N', type=str, nargs='?', default=os.getcwd()+'/') 
-	parser.add_argument('-o',	'--outdir', help='Specify output directory. End with a forward slash. Eg. /temp/fasta/; Default to use current directory.', metavar='N', type=str, nargs='?', default=os.getcwd()+'/')       
+	parser.add_argument('masking_file', help='The masking files with regions defined in an EMBL style tab-delimited file.', metavar='FILE', type=str, nargs='?')
+	parser.add_argument('-d',	'--dirpath', help='Specify input directory containing files. End with a forward slash. Eg. /temp/fasta/', metavar='DIR', type=str, nargs='?', default=os.getcwd()+'/') 
+	parser.add_argument('-o',	'--outdir', help='Specify output directory. End with a forward slash. Eg. /temp/fasta/; Default to use current directory.', metavar='DIR', type=str, nargs='?', default=os.getcwd()+'/')       
 	return parser.parse_args()
 
 ##########################################
